d695a4b10c523ac06bce0b7910a0ec5f9d8233c1
Michael Devery <michaeldevery@gmail.com>
2017-01-24 22:28:03 +0000

    from chapter 6

---
+++ countword.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "head/getword.h"
+
+#define MAXWORD 100
+
+struct node *addtree(struct node *, char *);
+void treeprint(struct node *);
+int getword(char *, int);
+struct node *talloc(void);
+
+struct node {
+    char *word;
+    int count;
+    struct node *left;
+    struct node *right;
+};
+
+/**
+ * word frequency count
+ */
+int main()
+{
+    struct node *root;
+    char word[MAXWORD];
+
+    root = NULL;
+    while (getword(word, MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            root = addtree(root, word);
+
+    treeprint(root);
+    return 0;
+}
+
+/**
+ * add a node with word, at or below parent
+ */
+struct node * addtree(struct node *parent, char *word)
+{
+    int cond;
+
+    if (parent == NULL) { // a new word
+        parent = talloc();
+        parent->word = strdup(word);
+        parent->count = 1;
+        parent->left = parent->right = NULL;
+    } else if ((cond = strcmp(word, parent->word)) == 0)
+        parent->count++;
+    else if (cond < 0)
+        parent->left = addtree(parent->left, word);
+    else
+        parent->right = addtree(parent->right, word);
+
+    return parent;
+}
+
+/**
+ * print tree of words in order
+ */
+void treeprint(struct node *p)
+{
+    if (p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+
+
+struct node * talloc(void)
+{
+    return (struct node *) malloc(sizeof(struct node));
+}

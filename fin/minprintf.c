13984495b52242047fe49b353bcc463715d5bdeb
Michael Devery <michaeldevery@gmail.com>
2017-01-31 08:36:53 +0000

    unmodified from c7

---
+++ fin/minprintf.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void minprintf(char *fmt, ...);
+
+int main()
+{
+    minprintf("%s %d\n", "abc", 50);
+
+}
+
+void minprintf(char *fmt, ...)
+{
+    va_list ap;
+    char* p;
+    char* sval;
+    int ival;
+    double dval;
+
+    va_start(ap, fmt);
+    
+    for (p = fmt; *p; p++) {
+        if (*p != '%') {
+            putchar(*p);
+            continue;
+        }
+        switch (*++p) {
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+                break;
+            case 's':
+                for (sval = va_arg(ap, char *); *sval; sval++)
+                    putchar(*sval);
+                break;
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+        
+}

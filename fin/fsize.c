28608f0dbf76062615fc6d9b70f748be57f29591
Michael Devery <michaeldevery@gmail.com>
2017-03-02 01:33:27 +0000

    fix fsize by removing low level calls :-( since they dont work on newer linux with directories

--- fin/fsize.c
+++ fin/fsize.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
 #include <sys/types.h>
+#include <dirent.h>
 #include <sys/stat.h>
-#include "directory.h"
+
+#define MAX_PATH 1024
 
 void dirwalk(char *, void (*fcn)(char*));
 int stat(const char*, struct stat *);
 void fsize(char *);
 
+/**
+ * modified version since open doesn't work on directory names any more
+*/
 int main(int argc, char** argv)
 {
     if (argc == 1)
         fsize(".");
     else
-        while (--argv > 0)
+        while (--argc > 0)
             fsize(*++argv);
     
     return 0;
@@ -36,23 +40,23 @@ void fsize(char* name)
 void dirwalk(char* dir, void (*fcn)(char*))
 {
     char name[MAX_PATH];
-    DirEnt* dp;
+    struct dirent* dp;
     DIR *dfd;
 
-    if ((dfd = open_dir(dir)) == NULL) {
+    if ((dfd = opendir(dir)) == NULL) {
         fprintf(stderr, "dirwalk can't open %s\n", dir);
         return;
     }
 
-    while ((dp = read_dir(dfd)) != NULL) {
-        if (strcmp(dp->name, ".") == 0 || strcmp(dp->name, "..") == 0)
+    while ((dp = readdir(dfd)) != NULL) {
+        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
             continue;
-        if (strlen(dir) + strlen(dp->name) + 2 > sizeof(name))
-            fprintf(stderr, "dirwalk: name %s/%s too long\n", dir, dp->name);
+        if (strlen(dir) + strlen(dp->d_name) + 2 > sizeof(name))
+            fprintf(stderr, "dirwalk: name %s/%s too long\n", dir, dp->d_name);
         else {
-            sprintf(name, "%s/%s", dir, dp->name);
+            sprintf(name, "%s/%s", dir, dp->d_name);
             (*fcn)(name);
         }
     }
-    close_dir(dfd);
+    closedir(dfd);
 }
--- fsize/localdir.h
+++
@@ -1,7 +0,0 @@
-#define DIRSIZ 14
-
-struct direct
-{
-    ino_t d_ino;
-    char d_name[DIRSIZ];
-};
